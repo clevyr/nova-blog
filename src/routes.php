e739e12006f9436be6f3fb50efe19fff0375b372
Grant Vinson <vinsongrant@gmail.com>
2022-04-26 16:25:53 -0500

    Updated more routes

--- src/routes.php
+++ src/routes.php
@@ -5,8 +5,21 @@ use Illuminate\Support\Facades\Route;
 /**
  * Return json data for a specific post by slug
  */
-Route::get(config('nova-blog.post_base_uri', '/post') . '/{slug}', function($slug) {
-   $model = app(config('nova-blog.post_model'));
-   $post = $model->where('slug', $slug)->where('is_published', 1)->first();
-   return json_encode($post);
+Route::get(
+    config('nova-blog.base_uri', '/blog') . config('nova-blog.post_base_uri', '/post') . '/{slug}',
+    function($slug)
+    {
+        $model = app(config('nova-blog.post_model'));
+        $post = $model->where('slug', $slug)->where('is_published', 1)->first();
+        return json_encode($post);
+    }
+);
+
+/**
+ * Return json data for all published posts
+ */
+Route::get(config('nova-blog.base_uri', '/blog') . '/get-published-posts', function() {
+    $model = app(config('nova-blog.post_model'));
+    $posts = $model->where('is_published', 1)->orderBy('title', 'asc')->get();
+    return json_encode($posts);
 });
